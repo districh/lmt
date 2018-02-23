c45e49a03dd8892302601bdbc467bc687671c0af
Liyuan <w3390221@sina.com>
2018-02-23 02:40:24 +0000

    Finish sign and change routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="/icon/test.jpg">
+            <a href="https://github.com/districh">
+                刻意练习，每日精进
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{route('about')}}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
